b0f336cc29811f0019ca31e5bb16de7fea8f5bc8
Avbdgvx <pfgargimondal@gmail.com>
2025-06-25 17:46:14 +0530

    about us page dynamic

--- src/pages/Aboutus/Aboutus.js
+++ src/pages/Aboutus/Aboutus.js
@@ -1,26 +1,67 @@
 import styles from "./Aboutus.module.css";
+import { useEffect, useState } from "react";
+import { useTitle } from "../../hooks/useTitle";
+import http from "../../http";
+import Loader from "../../component/Loader/Loader";
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 export const Aboutus = () => {
+  useTitle("About Us");
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const [AboutUsDetails, setAboutUsDetails] = useState({});
+    useEffect(() => {
+      const fetchAboutUsData = async () => {
+        setLoading(true);
+          try {
+          const getresponse = await http.get(`${process.env.REACT_APP_ABOUTUSAPI}`);
+          setAboutUsDetails(getresponse.data);
+  
+          } catch (error) {
+              console.error("Error fetching users:", error);
+          } finally{
+              setLoading(false);
+          }
+      }; 
+  
+      fetchAboutUsData();
+      }, []);
+
+      console.log(AboutUsDetails);
+  
+    if (loading) {
+      return <Loader />;
+    }
+
   return (
     <div>
+
       <main>
         <div className={styles.gdfbdfdfgdf554}>
           <div className={styles.dfgjhfgd}>
             <div className={`${styles.sdfghdfgdf} col-lg-6`}>
-              <h1 className="mt-0 mb-0">About Us</h1>
+              <h1 className="mt-0 mb-0">{AboutUsDetails.data?.banner_title && (AboutUsDetails.data.banner_title)}</h1>
 
               <p className="mt-3 mb-5">
-                Learn all about what CRM is, what it does and how it can improve
-                your customer relationships.
+                {AboutUsDetails.data?.banner_description && (AboutUsDetails.data.banner_description)}
               </p>
 
               <div className={styles.vbddds}>
-                <button className="btn125">Contact Us</button>
+                <Link to="/contact-us" className="btn125">
+                  Contact Us
+                </Link>
                 <button className="btn126">Get In Touch With Us</button>
               </div>
             </div>
 
             <div className={`${styles.dbfghsdf} col-lg-6`}>
-              <img src="/images/./affordable-banner.png" alt="" />
+              {AboutUsDetails.data?.banner_image && (
+                <img
+                  src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.banner_image}`}
+                  alt=""
+                />
+              )}
             </div>
           </div>
         </div>
@@ -31,42 +72,48 @@ export const Aboutus = () => {
               <div className="row">
                 <div className="col-lg-6">
                   <div className={styles.dfbgdfg}>
-                    <img src="./images/about-us.png" alt="" />
+                    {AboutUsDetails.data?.second_section_main_image && (
+                      <img
+                        src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.second_section_main_image}`}
+                        alt=""
+                      />
+                    )}
                   </div>
                 </div>
                 <div className="col-lg-6">
                   <div className={styles.dfbgfhdgjkdfnd54574}>
                     <div className={styles.sfbffsdf}>
-                      <h6>About Us</h6>
-                      <h2>Adding The Word Called Success to Your Brand</h2>
-                      <p>
-                        Sed ut perspiciatis unde omins error sit voluptatem
-                        accusa ntium doloremque laudantium totam rem aperiamea
-                        queipsa quae abillo inventore veritatis.Boost Your{" "}
-                      </p>
+                      {/* <h6>About Us</h6> */}
+                      <h2>{AboutUsDetails.data?.second_section_title && (AboutUsDetails.data.second_section_title)}</h2>
+                      
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: AboutUsDetails.data?.second_section_description && (AboutUsDetails.data.second_section_description),
+                        }}
+                      />
                     </div>
                     <div className={styles.sdfb5585dfgfg}>
                       <div className="row">
                         <div className="col-lg-6">
                           <div className={styles.sdbfdf}>
                             <p>
-                              <i className="fa-solid fa-square-check"></i> Boost
-                              Your Business
+                              <i className="fa-solid fa-square-check"></i> 
+                              {AboutUsDetails.data?.second_section_sub_title_one && (AboutUsDetails.data.second_section_sub_title_one)}
                             </p>
                             <p>
-                              <i className="fa-solid fa-square-check"></i> Boost
-                              Your Business
+                              <i className="fa-solid fa-square-check"></i> 
+                              {AboutUsDetails.data?.second_section_sub_title_two && (AboutUsDetails.data.second_section_sub_title_two)}
                             </p>
                           </div>
                         </div>
                         <div className="col-lg-6">
                           <p>
-                            <i className="fa-solid fa-square-check"></i> Boost Your
-                            Business
+                            <i className="fa-solid fa-square-check"></i>
+                            {AboutUsDetails.data?.second_section_sub_title_three && (AboutUsDetails.data.second_section_sub_title_three)}
                           </p>
                           <p>
-                            <i className="fa-solid fa-square-check"></i> Boost Your
-                            Business
+                            <i className="fa-solid fa-square-check"></i> 
+                            {AboutUsDetails.data?.second_section_sub_title_four && (AboutUsDetails.data.second_section_sub_title_four)}
                           </p>
                         </div>
                       </div>
@@ -76,19 +123,19 @@ export const Aboutus = () => {
                       <div className="row">
                         <div className="col-lg-4">
                           <div className={styles.dfgnbdf15gf}>
-                            <h2>247+</h2>
+                            <h2>{AboutUsDetails.data?.completed_projects && (AboutUsDetails.data.completed_projects)}+</h2>
                             <p>Completed Projects</p>
                           </div>
                         </div>
                         <div className="col-lg-4">
                           <div className={styles.dfgnbdf15gf}>
-                            <h2>54+</h2>
+                            <h2>{AboutUsDetails.data?.satisfied_customers && (AboutUsDetails.data.satisfied_customers)}+</h2>
                             <p>Satisfied Customers</p>
                           </div>
                         </div>
                         <div className="col-lg-4">
                           <div className={styles.dfgnbdf15gf}>
-                            <h2>43+</h2>
+                            <h2>{AboutUsDetails.data?.winning_awards && (AboutUsDetails.data.winning_awards)}+</h2>
                             <p>Winning Awards</p>
                           </div>
                         </div>
@@ -104,28 +151,36 @@ export const Aboutus = () => {
         <div className={styles.fjbdfhgd}>
           <div className="custom-container">
             <div className={styles.fgdffdg}>
-              <h2>Why Do You Works with Us</h2>
-              <p>
-                It is a long established fact that a reader will be distracted
-                by the readable content of a page when looking at its layout
-              </p>
+              <h2>{AboutUsDetails.data?.third_section_title && (AboutUsDetails.data.third_section_title)}</h2>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: AboutUsDetails.data?.third_section_description && (AboutUsDetails.data.third_section_description),
+                }}
+              />
             </div>
             <div className={styles.dfsdfsdfgdfg}>
               <div className="row">
                 <div className="col-lg-5">
                   <div className={styles.sdfbh55dfgsd}>
-                    <img src="./images/abouta (1).png" alt="" />
+                    {AboutUsDetails.data?.third_section_sub_image_one && (
+                      <img
+                        src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.third_section_sub_image_one}`}
+                        alt=""
+                      />
+                    )}
                   </div>
                 </div>
                 <div className="col-lg-7">
                   <div className={styles.fdghdf55gdf}>
-                    <h2>Where does it come from?</h2>
-                    <p>
-                      There are many variations of passages of Lorem Ipsum
-                      available, but the majority have suffered alteration in
-                      some form, by injected humour.
-                    </p>
-                    <button>Contact Us</button>
+                    <h2>{AboutUsDetails.data?.third_section_sub_title_one && (AboutUsDetails.data.third_section_sub_title_one)}</h2>
+                    <div
+                        dangerouslySetInnerHTML={{
+                          __html: AboutUsDetails.data?.third_section_sub_description_one && (AboutUsDetails.data.third_section_sub_description_one),
+                        }}
+                      />
+                    <button onClick={() => navigate('/contact-us')}>
+                        Contact Us
+                    </button>
                   </div>
                 </div>
               </div>
@@ -135,18 +190,25 @@ export const Aboutus = () => {
               <div className="row">
                 <div className="col-lg-5">
                   <div className={styles.sdfbh55dfgsd2}>
-                    <img src="./images/abouta (2).png" alt="" />
+                    {AboutUsDetails.data?.third_section_sub_image_two && (
+                      <img
+                        src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.third_section_sub_image_two}`}
+                        alt=""
+                      />
+                    )}
                   </div>
                 </div>
                 <div className="col-lg-7">
                   <div className={styles.fdghdf55gdf2}>
-                    <h2>Where does it come from?</h2>
-                    <p>
-                      There are many variations of passages of Lorem Ipsum
-                      available, but the majority have suffered alteration in
-                      some form, by injected humour.
-                    </p>
-                    <button>Contact Us</button>
+                    <h2>{AboutUsDetails.data?.third_section_sub_title_two && (AboutUsDetails.data.third_section_sub_title_two)}</h2>
+                    <div
+                        dangerouslySetInnerHTML={{
+                          __html: AboutUsDetails.data?.third_section_sub_description_two && (AboutUsDetails.data.third_section_sub_description_two),
+                        }}
+                      />
+                    <button onClick={() => navigate('/contact-us')}>
+                        Contact Us
+                    </button>
                   </div>
                 </div>
               </div>
@@ -161,25 +223,30 @@ export const Aboutus = () => {
                 <div className="col-lg-6">
                   <div className={styles.sdfsdf325544}>
                     <div className={styles.dfbgdf555}>
-                      <h6>WHY CHOOSE US</h6>
-                      <h2>We're the Best Choice for Your Handyman </h2>
+                      <h6>{AboutUsDetails.data?.fourth_section_heading && (AboutUsDetails.data.fourth_section_heading)}</h6>
+                      <h2>{AboutUsDetails.data?.fourth_section_title && (AboutUsDetails.data.fourth_section_title)}</h2>
                     </div>
                     <div className={styles.dfdfhdf565}>
                       <div className={styles.dsdfsd}>
                         <div className="row">
                           <div className="col-lg-2">
                             <div className={styles.dfbdfg328}>
-                              <img src="./images/abouicon (1).png" alt="" />
+                              {AboutUsDetails.data?.fourth_section_sub_image_one && (
+                                <img
+                                  src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.fourth_section_sub_image_one}`}
+                                  alt=""
+                                />
+                              )}
                             </div>
                           </div>
                           <div className="col-lg-10">
                             <div className={styles.dfgbdfgdf}>
-                              <h5>Experienced Professionals</h5>
-                              <p>
-                                Our team is made up of skilled and certified
-                                handymen with extensive experience across
-                                various trades.
-                              </p>
+                              <h5>{AboutUsDetails.data?.fourth_section_sub_title_one && (AboutUsDetails.data.fourth_section_sub_title_one)}</h5>
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html: AboutUsDetails.data?.fourth_section_sub_description_one && (AboutUsDetails.data.fourth_section_sub_description_one),
+                                }}
+                              />
                             </div>
                           </div>
                         </div>
@@ -189,17 +256,22 @@ export const Aboutus = () => {
                         <div className="row">
                           <div className="col-lg-2">
                             <div className={styles.dfbdfg328}>
-                              <img src="./images/abouicon (2).png" alt="" />
+                              {AboutUsDetails.data?.fourth_section_sub_image_two && (
+                                <img
+                                  src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.fourth_section_sub_image_two}`}
+                                  alt=""
+                                />
+                              )}
                             </div>
                           </div>
                           <div className="col-lg-10">
                             <div className={styles.dfgbdfgdf}>
-                              <h5>Quality Workmanship</h5>
-                              <p>
-                                Our team is made up of skilled and certified
-                                handymen with extensive experience across
-                                various trades.
-                              </p>
+                              <h5>{AboutUsDetails.data?.fourth_section_sub_title_two && (AboutUsDetails.data.fourth_section_sub_title_two)}</h5>
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html: AboutUsDetails.data?.fourth_section_sub_description_two && (AboutUsDetails.data.fourth_section_sub_description_two),
+                                }}
+                              />
                             </div>
                           </div>
                         </div>
@@ -209,17 +281,22 @@ export const Aboutus = () => {
                         <div className="row">
                           <div className="col-lg-2">
                             <div className={styles.dfbdfg328}>
-                              <img src="./images/abouicon (3).png" alt="" />
+                              {AboutUsDetails.data?.fourth_section_sub_image_three && (
+                                <img
+                                  src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.fourth_section_sub_image_three}`}
+                                  alt=""
+                                />
+                              )}
                             </div>
                           </div>
                           <div className="col-lg-10">
                             <div className={styles.dfgbdfgdf}>
-                              <h5>Fully Insured</h5>
-                              <p>
-                                Our team is made up of skilled and certified
-                                handymen with extensive experience across
-                                various trades.
-                              </p>
+                              <h5>{AboutUsDetails.data?.fourth_section_sub_title_three && (AboutUsDetails.data.fourth_section_sub_title_three)}</h5>
+                              <div
+                                dangerouslySetInnerHTML={{
+                                  __html: AboutUsDetails.data?.fourth_section_sub_description_three && (AboutUsDetails.data.fourth_section_sub_description_three),
+                                }}
+                              />
                             </div>
                           </div>
                         </div>
@@ -230,15 +307,19 @@ export const Aboutus = () => {
                 <div className="col-lg-6">
                   <div className={styles.fghdfgdf6565g}>
                     <div className={styles.jdfvgdf}>
-                      <p>
-                        At Fixit, we understand that your home is your
-                        sanctuary, and we’re committed to keeping it in top
-                        shape. Here’s why we’re the best choice for all your
-                        handyman needs:{" "}
-                      </p>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: AboutUsDetails.data?.fourth_section_description && (AboutUsDetails.data.fourth_section_description),
+                        }}
+                      />
                     </div>
                     <div className={styles.dfhdsfsf}>
-                      <img src="./images/about85.jpg" alt="" />
+                      {AboutUsDetails.data?.fourth_section_image && (
+                        <img
+                          src={`${AboutUsDetails.image_url}/${AboutUsDetails.data.fourth_section_image}`}
+                          alt=""
+                        />
+                      )}
                     </div>
                   </div>
                 </div>
